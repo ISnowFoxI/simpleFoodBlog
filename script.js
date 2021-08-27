296d94008bf1c656f6141799be55d888616638ec
SnowFox <65253170+ISnowFoxI@users.noreply.github.com>
2021-08-27 20:02:28 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuButton = document.getElementById("menuButton")
+const navbar = document.getElementById("navBar")
+
+
+menuButton.addEventListener("click", ()=> { 
+    if(menuButton.checked) { 
+        navbar.classList.add("isOpen")
+    }
+    else { 
+        navbar.classList.remove("isOpen")
+    }
+})
