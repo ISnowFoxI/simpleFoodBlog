a200a9064f63ed9b8983317dcbcd1ffc3dc37e35
SnowFox <65253170+ISnowFoxI@users.noreply.github.com>
2021-08-29 04:17:09 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ menuButton.addEventListener("click", ()=> {
         navbar.classList.remove("isOpen")
     }
 })
+
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.glider'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        scrollLock: true,
+        rewind:true,
+        arrows: {
+          prev: '.gliderPrev',
+          next: '.gliderNext'
+        },
+        responsive: [
+          {
+            breakpoint: 700,
+            settings: {
+              slidesToShow: "auto",
+              slidesToScroll: 'auto',
+              itemWidth:350,
+              duration: 0.5
+            }
+          }
+        ]
+      });
+  })
